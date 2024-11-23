d6dd12701eda1c378257d536a89d85c0311bd0be
GBlecher <gilanblecher@gmail.com>
2024-11-23 16:38:49 +0200

    user & category com1

--- Python/Py_classes/cls_categories.py
+++ Python/Py_classes/cls_categories.py
@@ -1,11 +1,21 @@
 class Categories:
-    def __init__(self, category_name): #Initializes a category object.
+    def __init__(self, category_name, description): #Initializes a category object.
         self.category_name = category_name
+        self.description = description
     def create_category(self, db_connection): #Inserts a new category.
         cursor = db_connection.cursor()
-        cursor.execute("""INSERT INTO categories(name)
-                       VALUES (%s)""",(self.category_name))
-        db_connection.commit()
+        try:
+            cursor.execute("""INSERT INTO categories(name)
+                        VALUES (%s,%s)""",(self.category_name, self.description))
+            db_connection.commit()
+            print("Category created successfully!")
+        except Exception as error:
+            db_connection.rollback()  # Rollback in case of error
+            print(f"Error occurred - While creating category: {error}")
+        finally:
+            cursor.close()  # Close the cursor
+            
+        
     @classmethod
     def get_all_categories(cls, db_connection):# Fetches all categories.
         cursor = db_connection.cursor()
@@ -16,12 +26,23 @@ class Categories:
 
     def update_category(self, db_connection, category_id): #Updates a category.
         cursor = db_connection.cursor()
-        cursor.execute()
-        db_connection.commit()
+        try:
+            cursor.execute(""" 
+                        UPDATE categories 
+                        SET name = %s
+                        WHERE category_id = %s""",
+                        (self.category_name, category_id))
+            db_connection.commit()
+            print("Category updated successfully!")
+        except Exception as error:
+            db_connection.rollback()  # Rollback in case of error
+            print(f"Error occurred - While updating category: {error}")
+        finally:
+            cursor.close()  # Close the cursor
     @classmethod
     def delete_category(cls, db_connection, category_id): #Deletes a category.
         cursor = db_connection.cursor()
-        cursor.execute()
+        cursor.execute(" DELETE FROM users WHERE id = %s",(category_id))
     
     
     
--- Python/Py_classes/cls_user.py
+++ Python/Py_classes/cls_user.py
@@ -10,33 +10,53 @@ class User:
         
     def create_user(self, db_connection):# Inserts a new user into the database.
         cursor = db_connection.cursor()
-        cursor.execute("""
+        try:
+            cursor.execute("""
                     INSERT INTO users (username, first_name, last_name, email, password)
                     VALUES(%s,%s,%s,%s,%s)""",
                     (self.username, self.first_name, self.last_name, self.email, self.password))
-        db_connection.commit()
+            db_connection.commit()
+            print("User created successfully!")
+        except Exception as error:
+            db_connection.rollback()  # Rollback in case of error
+            print(f"Error occurred  - While creating user: {error}")
+        finally:
+            cursor.close()  # Close the cursor
     
-            
     @classmethod
     def get_user_by_id(cls, db_connection, user_id): #Fetches a user by ID.
         cursor = db_connection.cursor()
-        cursor.execute("Select * from users WHERE id = %s",(user_id))
-        row = cursor.fetchone()
-        if row:
-            return cls(row[1], row[2], row[3], row[4], row[5],)
-        return None
-        
-    def update_user(self, db_connection, user_id): #Updates user information.
+        try:
+            cursor.execute("Select * from users WHERE id = %s",(user_id))
+            row = cursor.fetchone()
+            if row:
+                return cls(row[1], row[2], row[3], row[4], row[5],)
+            return None
+        finally:
+            cursor.close()
+            
+    def update_user(self, db_connection, new_username, new_first_name, new_last_name, new_email, new_password, user_id): #Updates user information.
         cursor = db_connection.cursor()
-        cursor.execute(""" 
-                       UPDATE users 
-                       SET Username = %s,first_name= %s, last_name= %s, email= %s, password= %s""",
-                       (self.username, self.first_name, self.last_name, self.email, self.password))
-        db_connection.commit()
-        
+        try:
+            cursor.execute(""" 
+                        UPDATE users 
+                        SET Username = %s,first_name= %s, last_name= %s, email= %s, password= %s 
+                        WHERE user_id = %s""",
+                        (new_username =, new_first_name, new_last_name, new_email, new_password, user_id))
+            db_connection.commit()
+            print("User updated successfully!")
+        except Exception as error:
+            db_connection.rollback()  # Rollback in case of error
+            print(f"Error occurred - While updating user: {error}")
+        finally:
+            cursor.close()  # Close the cursor
+            
     def delete_user(cls, db_connection, user_id):# Deletes a user.
-         cursor = db_connection.cursor()
-         cursor.execute(" DELETE FROM users WHERE id = %s",(user_id))
-         db_connection.commit()
+        cursor = db_connection.cursor()
+        try:
+            cursor.execute(" DELETE FROM users WHERE id = %s",(user_id))
+            db_connection.commit()
+        finally:
+            cursor.close()
          
          
