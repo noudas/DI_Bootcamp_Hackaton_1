4081a1a0c0a48ae6bca5c7c3a5749305d9cc57d5
GBlecher <gilanblecher@gmail.com>
2024-11-24 10:02:35 +0200

    merge wtf

--- Python/Py_classes/cls_deposits.py
+++ Python/Py_classes/cls_deposits.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import psycopg2
 from config import Config
 from cls_DB_connect import DB_Connect
@@ -63,36 +62,3 @@ class Deposits:
             print("Deposit deleted successfully!")
         except Exception as error:
             print(f"Error occurred while deleting deposit: {error}")
-=======
-class Deposits:
-    def __init__(self, amount, deposit_date, notes): #Initializes a deposit object.
-        self.amount = amount
-        self.deposit_date = deposit_date
-        self.notes = notes
-    def add_deposit(self, db_connection): #Inserts a new deposit record.
-        cursor = db_connection.cursor()
-        try:
-            cursor.execute("""INSERT INTO deposits (amount, deposit_date, notes)
-                       VALUES (%s,%s,%s)""",(self.amount, self.deposit_date, self.notes))
-            db_connection.commit()
-            print("Deposit added successfully!")
-        except Exception as error:
-            db_connection.rollback()  # Rollback in case of error
-            print(f"Error occurred - While adding deposits: {error}")
-        finally:
-            cursor.close()  # Close the cursor    
-    @classmethod
-    def get_deposits(cls, db_connection): #Fetches all deposits.
-        cursor = db_connection.cursor()
-        try:    
-            cursor.execute("SELECT * FROM deposits")
-            deposits = cursor.fetchall()
-            return deposits
-        finally:
-            cursor.close()  # Close the cursor    
-
-    @classmethod
-    def delete_deposit(cls, db_connection, deposit_id): #Deletes a deposit record
-        cursor = db_connection.cursor()
-        cursor.execute()
->>>>>>> 73aad84698f2328fa75797dc7a71397350b654f3
--- Python/Py_classes/cls_expenses.py
+++ Python/Py_classes/cls_expenses.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import psycopg2
 from config import Config
 from cls_DB_connect import DB_Connect
@@ -90,53 +89,3 @@ class Expenses:
             print("Expense deleted successfully!")
         except Exception as error:
             print(f"Error occurred while deleting an expense: {error}")
-=======
-class Expenses:
-    def __init__(self, amount, category_name, notes): #Initializes an expense object.
-       self.amount = amount
-       self.category_name = category_name.lower()
-       self.notes = notes
-    def add_expense(self, db_connection): #Inserts a new expense.
-        cursor = db_connection.cursor()
-        try:
-            cursor.execute("""INSERT INTO expenses (amount, category_id, notes)
-                       VALUES (%s,%s,%s)""",(self.amount, self.category_name, self.category_id))
-            db_connection.commit()
-            print("Expense added successfully!")
-        except Exception as error:
-            db_connection.rollback()  # Rollback in case of error
-            print(f"Error occurred - While adding an expense: {error}")
-        finally:
-            cursor.close()  # Close the cursor
-    @classmethod
-    def get_expense_by_id(cls, db_connection, expense_id): #Fetches an expense by ID.
-        cursor = db_connection.cursor()
-        try:
-            cursor.execute("SELECT * FROM expenses WHERE expense_id = %s",(expense_id))
-        finally:
-            cursor.close()  # Close the cursor
-    
-    @classmethod
-    def get_all_expenses(cls, db_connection): #Fetches an expense by ID.
-        cursor = db_connection.cursor()
-        try:    
-            cursor.execute("SELECT * FROM expenses")
-            expenses = cursor.fetchall()
-            return expenses
-        finally:
-            cursor.close()  # Close the cursor          
-     
-    def get_expenses_by_category(self, db_connection ): #Fetches expenses for a specific category by name.
-        cursor = db_connection.cursor()
-        try:
-            cursor.execute("SELECT * FROM expenses WHERE category_id = %s",(self.category_name))
-        finally:
-            cursor.close()  # Close the cursor
-    @classmethod
-    def delete_expense(cls, db_connection, expense_id): #Deletes an expense.
-        cursor = db_connection.cursor()
-        try:
-            cursor.execute("DELETE FROM expenses WHERE id = %s",(expense_id))
-        finally:
-            cursor.close()  # Close the cursor
->>>>>>> 73aad84698f2328fa75797dc7a71397350b654f3
