56e33075d087dc4aef718021d6927cdcfa49035c
GBlecher <gilanblecher@gmail.com>
2024-11-24 16:19:43 +0200

    deposits menu and xlass fix

--- Python/Py_classes/cls_deposits.py
+++ Python/Py_classes/cls_deposits.py
@@ -3,29 +3,27 @@ from config import Config
 from Py_classes import DB_Connect
 
 class Deposits:
-    def __init__(self, db: DB_Connect, user_id, amount, deposit_date, description=None):
+    def __init__(self, db: DB_Connect, user_id, amount, description=None):
         """
         Initializes a deposit with the provided parameters.
 
         :param db: An instance of DB_Connect for interacting with the database.
         :param user_id: The user associated with the deposit.
         :param amount: The amount of the deposit.
-        :param deposit_date: The date of the deposit.
         :param description: An optional description for the deposit.
         """
         self.db = db
         self.user_id = user_id
         self.amount = amount
-        self.deposit_date = deposit_date
         self.description = description
 
     def add_deposit(self):
         """
         Adds a new deposit to the database.
         """
-        query = """INSERT INTO deposits (user_id, amount, deposit_date, description)
-                   VALUES (%s, %s, %s, %s)"""
-        values = (self.user_id, self.amount, self.deposit_date, self.description)
+        query = """INSERT INTO deposits (user_id, amount, description)
+                   VALUES (%s, %s, %s)"""
+        values = (self.user_id, self.amount, self.description)
         try:
             self.db.execute_query(query, values)
             print("Deposit added successfully!")
@@ -33,14 +31,15 @@ class Deposits:
             print(f"Error occurred while adding deposit: {error}")
 
     @classmethod
-    def get_deposits(cls, db: DB_Connect):
+    def get_deposits(cls, db: DB_Connect,user_id):
         """
-        Fetches all deposit records from the database.
+        Fetches all deposit records from the database for a user.
 
         :param db: An instance of DB_Connect for interacting with the database.
         :return: A list of Deposit objects.
         """
-        query = "SELECT deposit_id, user_id, amount, deposit_date, description FROM deposits"
+        query = "SELECT deposit_id, user_id, amount, description FROM deposits WHERE user_id = %s"
+        values = (user_id)
         results = db.fetch_results(query)
         
         # Return a list of Deposit objects
@@ -56,7 +55,7 @@ class Deposits:
         :param deposit_id: The ID of the deposit to delete.
         """
         query = "DELETE FROM deposits WHERE deposit_id = %s"
-        values = (deposit_id,)
+        values = (deposit_id)
         try:
             db.execute_query(query, values)
             print("Deposit deleted successfully!")
